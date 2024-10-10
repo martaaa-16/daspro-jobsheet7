13509c5d1fca5ea38ea862edb4c3d8b3bfb71996
martaaa-16 <martaprama@gmail.com>
2024-10-10 10:09:07 +0700

    percobaan 3

--- SiakadFor15.java
+++ SiakadFor15.java
@@ -6,6 +6,7 @@ public class SiakadFor15 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Nilai mahasiswa ke-" + i + ": ");
@@ -16,9 +17,16 @@ public class SiakadFor15 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
-
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
     }
 }
